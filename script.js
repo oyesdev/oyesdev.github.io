6115e8753675aa1d36e74c3d7022a2a4f6a1ef0c
Divyansh <divyanshsrivastava20@gmail.com>
2020-11-07 14:08:38 +0530

    added animations and made the site responsive

--- script.js
+++ script.js
@@ -19,7 +19,33 @@ const navSlide = () => {
         burger.classList.toggle('toggle');
     });
 }
+
+const typingCarousel = () => {
+    const texts = ['web developer', 'designer'];
+    let count = 0;
+    let index = 0;
+    let currentText = '';
+    let letter = '';
+
+    (function type(){
+
+        if(count === texts.length){
+            count = 0;
+        }
+        currentText = texts[count];
+        letter = currentText.slice(0, ++index);
+
+        document.querySelector('.typing').textContent = letter;
+        if(letter.length == currentText.length){
+            count++;
+            index = 0;
+        }
+        setTimeout(type, 400);
+    }());
+}
+
 const app = () => {
     navSlide();
+    typingCarousel();
 }
 app();
